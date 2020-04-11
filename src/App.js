b4d1406b16c8105dc069e979ad9b3b00a352a848
Ian Holmes <ihh@berkeley.edu>
2020-04-10 23:08:56 -0700

    feedback on tree progress

--- src/App.js
+++ src/App.js
@@ -183,8 +183,8 @@ class App extends Component {
         traverse (newickTree)
         data.root = getName (newickTree)
       } else {  // no Newick tree was specified, so build a quick-and-dirty distance matrix with Jukes-Cantor, and get a tree by neighbor-joining
-        console.warn ('Estimating phylogenetic tree...')
         const taxa = Object.keys(data.rowData).sort(), seqs = taxa.map ((taxon) => data.rowData[taxon])
+        console.warn ('Estimating phylogenetic tree (' + taxa.length + ' taxa)...')
         const distMatrix = JukesCantor.calcFiniteDistanceMatrix (seqs)
         const rnj = new RapidNeighborJoining.RapidNeighborJoining (distMatrix, taxa.map ((name) => ({ name })))
         rnj.run()
