9e130959645689a3b0d9523ae0a9c46199800f19
Ian Holmes <ihh@berkeley.edu>
2020-04-11 18:01:49 -0700

    added build directory

---
+++ build/precache-manifest.d9e2b66378af4b4059b584714443127a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36626fe132c62f185a074a58ad3cd437",
+    "url": "/1b67139851fbce868382.worker.js"
+  },
+  {
+    "revision": "9ac26e5d7b74e622b49882f370e380b4",
+    "url": "/1b67139851fbce868382.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "8827c7d454e4cf21c45cef2a859ea80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77cbbc67fda1a5d1a975",
+    "url": "/static/css/main.4974d75f.chunk.css"
+  },
+  {
+    "revision": "9c9d20dff88964eb03d0",
+    "url": "/static/js/2.e010b787.chunk.js"
+  },
+  {
+    "revision": "1b2df138df645c9ba03c530ebe7d17e6",
+    "url": "/static/js/2.e010b787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77cbbc67fda1a5d1a975",
+    "url": "/static/js/main.08c47107.chunk.js"
+  },
+  {
+    "revision": "54239437680fa75bac38",
+    "url": "/static/js/runtime-main.b45ba4e9.js"
+  }
+]);
