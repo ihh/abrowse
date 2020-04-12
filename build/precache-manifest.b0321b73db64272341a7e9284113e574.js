8ed208ec1173c9aa7686ffa652cf5314cb864365
Ian Holmes <ihh@berkeley.edu>
2020-04-11 22:35:42 -0700

    rebuild

---
+++ build/precache-manifest.b0321b73db64272341a7e9284113e574.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980ec537532a818c34a09900dbd442c1",
+    "url": "./49e0c98df6571801ee9b.worker.js"
+  },
+  {
+    "revision": "9ac26e5d7b74e622b49882f370e380b4",
+    "url": "./49e0c98df6571801ee9b.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61ab049a82c23ce8296637ea339c43e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85c3a7f0ff04e8f3b29e",
+    "url": "./static/css/main.64b704ce.chunk.css"
+  },
+  {
+    "revision": "c343f00d81cb9b9effd6",
+    "url": "./static/js/2.135da0ae.chunk.js"
+  },
+  {
+    "revision": "1b2df138df645c9ba03c530ebe7d17e6",
+    "url": "./static/js/2.135da0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c3a7f0ff04e8f3b29e",
+    "url": "./static/js/main.d196a5f4.chunk.js"
+  },
+  {
+    "revision": "27d7cb8b11de591f5bd5",
+    "url": "./static/js/runtime-main.9e6acb83.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.321d9664e20d8b4c1467b2624c791bbb.js"
+  "./precache-manifest.b0321b73db64272341a7e9284113e574.js"
 );
 
 self.addEventListener('message', (event) => {
